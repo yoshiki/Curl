0d556030f26ff916632665d54ce181992ed967a6
Yoshiki Kurihara <clouder@gmail.com>
2016-09-30 18:54:24 +0900

    Remove unused dependency

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "Curl",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/CCurl.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 12),
     ]
 )
